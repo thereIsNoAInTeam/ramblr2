d23156c2e6ac34b12e9089ecbf0232c91db5d80c
Christopher Rescorla <cgrescorla@gmail.com>
2017-02-15 15:19:33 -0700

    Google done, next would be email, probably at school tomorrow

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,15 +1,53 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {NavController, ToastController} from 'ionic-angular';
+import {Subscription} from "rxjs";
 
-import { NavController } from 'ionic-angular';
+import {UserDatabase} from "../../providers/user-database";
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+    selector: 'page-home',
+    templateUrl: 'home.html'
 })
 export class HomePage {
+    loggedIn: boolean;
+    loggedInSubscription: Subscription;
 
-  constructor(public navCtrl: NavController) {
+    constructor(public navCtrl: NavController, private userDatabase: UserDatabase, private toastCtrl: ToastController) {
 
-  }
+    }
 
+    ionViewDidLoad(): void {
+        this.loggedInSubscription = this.userDatabase.amLoggedIn$.subscribe(
+            loggedStatus => this.loggedIn = loggedStatus
+        );
+    }
+
+    loginToGoogle(): void {
+        this.userDatabase.googleLogin()
+            .then(() => this.signInSuccess());
+    }
+
+    logoutOfGoogle(): void {
+        this.userDatabase.googleLogout();
+    }
+
+    loginWithEmail(): void {
+        console.log("Email login!");
+    }
+
+    logoutOfEmail(): void {
+        console.log("Email logout! Probably the same as Google logout!");
+    }
+
+    registerEmail(): void {
+        console.log("Registration time! Maybe the same as Email login!");
+    }
+
+    private signInSuccess(): void {
+        let toast = this.toastCtrl.create({
+            message: "Sign in successful!",
+            duration: 2000
+        });
+        toast.present();
+    }
 }
--- src/providers/user-database.ts
+++ src/providers/user-database.ts
@@ -1,18 +1,37 @@
-import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import {Injectable} from '@angular/core';
+import {Http} from '@angular/http';
 import 'rxjs/add/operator/map';
+import {FirebaseAuthState, AngularFire, AngularFireAuth, AuthProviders, AuthMethods} from "angularfire2";
+import {Subject} from "rxjs";
 
-/*
-  Generated class for the UserDatabase provider.
-
-  See https://angular.io/docs/ts/latest/guide/dependency-injection.html
-  for more info on providers and Angular 2 DI.
-*/
 @Injectable()
 export class UserDatabase {
+    private authState: FirebaseAuthState;
 
-  constructor(public http: Http) {
-    console.log('Hello UserDatabase Provider');
-  }
+    amLoggedIn = new Subject<any>();
+    amLoggedIn$ = this.amLoggedIn.asObservable();
 
+    constructor(public http: Http, private af: AngularFire, private auth$: AngularFireAuth) {
+        this.af.auth.subscribe(state => {
+            this.authState = state;
+
+            // this will only listen to see if someone is logged in or not
+            this.amLoggedIn.next(this.authenticated);
+        });
+    }
+
+    get authenticated(): boolean {
+        return this.authState != null;
+    }
+
+    googleLogin(): firebase.Promise<FirebaseAuthState> {
+        return this.auth$.login({
+            provider: AuthProviders.Google,
+            method: AuthMethods.Popup
+        })
+    }
+
+    googleLogout(): void {
+        this.auth$.logout();
+    }
 }
