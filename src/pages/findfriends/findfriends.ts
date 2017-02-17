2892d5a68d0ee5e413387429a7b2706365d51593
matthewculp <mattculp08@gmail.com>
2017-02-17 10:19:47 -0700

    Find Friends Component Added and Search bar put on individual page

---
+++ src/pages/findfriends/findfriends.ts
@@ -0,0 +1,50 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+@Component({
+  selector: 'page-findfriends',
+  templateUrl: 'findfriends.html'
+})
+export class FindfriendsPage {
+
+  searchQuery: string = '';
+  items: string[];
+  searching: boolean = false;
+
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  this.initializeItems();
+}
+
+initializeItems() {
+  this.items = [
+    "BillGates",
+    "Johnny Fawkes",
+    "Bill Nye",
+    "Lady GoGo",
+    "Miley Cyrus"
+  ];
+}
+
+getItems(ev: any) {
+  // Reset items back to all of the items
+  this.initializeItems();
+
+  // set val to the value of the searchbar
+  let val = ev.target.value;
+
+  // if the value is an empty string don't filter the items
+  if (val && val.trim() != '') {
+    this.searching = true;
+    this.items = this.items.filter((item) => {
+      return (item.toLowerCase().indexOf(val.toLowerCase()) > -1);
+    })
+  }
+  else {
+    this.searching = false;
+
+  }
+}
+
+
+}
