b2acd654c34886e929c376c43bc2af93d95d05fc
Christopher Rescorla <cgrescorla@gmail.com>
2017-02-23 10:24:32 -0700

    changed Home page, but nav needs to change

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,12 +1,10 @@
 import {Component, ViewChild} from '@angular/core';
 import {NavController, ToastController, ModalController, Nav} from 'ionic-angular';
-import {Subscription} from "rxjs";
 
 import {UserDatabase} from "../../providers/user-database";
 import {RegisterPage} from "../register/register";
 import {LoginPage} from "../login/login";
-import {FirebaseListObservable} from "angularfire2";
-import {ProfilePage} from "../profile/profile";
+import {FeedPage} from "../feed/feed";
 
 @Component({
     selector: 'page-home',
@@ -15,29 +13,11 @@ import {ProfilePage} from "../profile/profile";
 export class HomePage {
     @ViewChild(Nav) nav: Nav;
 
-    loggedIn: boolean;
-    loggedInSubscription: Subscription;
-    usersSubscription: Subscription;
-    email: string;
-    password: string;
-    users: FirebaseListObservable<any[]>;
-
     constructor(public navCtrl: NavController, private userDatabase: UserDatabase, private toastCtrl: ToastController, private modalCtrl: ModalController) {
 
     }
 
     ionViewDidLoad(): void {
-        this.loggedInSubscription = this.userDatabase.amLoggedIn$.subscribe(
-            loggedStatus => {
-                this.loggedIn = loggedStatus;
-                if (this.loggedIn) {
-                    this.navCtrl.setRoot(ProfilePage);
-                }
-            }
-        );
-        this.usersSubscription = this.userDatabase.myUsers$.subscribe(
-            myUsers => this.users = myUsers
-        )
     }
 
     loginToGoogle(): void {
@@ -47,11 +27,6 @@ export class HomePage {
                 console.log(reason);
                 this.signInFailed();
             });
-    }
-
-    // use for sign out regardless of sign in method...
-    logoutOfGoogle(): void {
-        this.userDatabase.googleLogout();
     }
 
     registration(): void {
@@ -66,7 +41,7 @@ export class HomePage {
 
     private signInSuccess(): void {
         this.userDatabase.createUser();
-        this.navCtrl.setRoot(ProfilePage);
+        this.navCtrl.setRoot(FeedPage);
         let toast = this.toastCtrl.create({
             message: "Sign in successful!",
             duration: 2000
