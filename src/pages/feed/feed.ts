741ad3e1eb2bbeb8012fe9aed9bff96cac7e27a7
ninzhan <ninzhan5@gmail.com>
2017-03-01 08:03:09 -0700

    Commit to merge

--- src/pages/feed/feed.ts
+++ src/pages/feed/feed.ts
@@ -3,32 +3,21 @@ import {Component} from '@angular/core';
 import {NavController} from 'ionic-angular';
 import {PostPage} from "../post/post";
 import {UserDatabase} from "../../providers/user-database";
-
+import {Subscription} from "rxjs";
 
 @Component({
     selector: 'page-feed',
     templateUrl: 'feed.html'
 })
 export class FeedPage {
-    postAbout: string;
-    postDate: string;
-    postComments: string;
-    postPic: any;
+    postArray: any[];
 
 
     constructor(public navCtrl: NavController, private userDatabase: UserDatabase) {
-        //Camera.getPicture(C).then((imageData) => {
 
-        // imageData is either a base64 encoded string or a file URI
-        // If it's base64:
-        //    let base64Image = 'data:image/jpeg;base64,' + imageData;
-        //}, (err) => {
-        // Handle error
-        //});
     }
 
     newPost(): void {
         this.navCtrl.push(PostPage);
-
     }
 }
