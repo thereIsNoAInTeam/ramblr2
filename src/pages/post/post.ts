c80a616a24629b94b56df7f11d52e772c871f7ed
matthewculp <mattculp08@gmail.com>
2017-03-03 07:39:37 -0700

    Style Changes + Google Profile Pic implemented

--- src/pages/post/post.ts
+++ src/pages/post/post.ts
@@ -1,5 +1,5 @@
 import {Component} from '@angular/core';
-import {NavController, NavParams} from 'ionic-angular';
+import {NavController, NavParams, AlertController} from 'ionic-angular';
 import {UserDatabase} from "../../providers/user-database";
 import {Subscription} from "rxjs";
 
@@ -12,7 +12,7 @@ export class PostPage {
     postArray: any[];
     postSubscription: Subscription;
 
-    constructor(public navCtrl: NavController, public navParams: NavParams, private userDatabase: UserDatabase) {
+    constructor(public navCtrl: NavController, public navParams: NavParams, private alertCtrl: AlertController, private userDatabase: UserDatabase) {
         this.postSubscription = this.userDatabase.myPosts$.subscribe(posts => {
             this.postArray = posts;
         });
@@ -29,7 +29,21 @@ export class PostPage {
         else {
             this.postArray = [{post: this.post, time: Date.now()}];
         }
-        this.navCtrl.pop();
-        this.userDatabase.updatePosts(this.postArray)
+        if (this.post.length < 500) {
+            let alert = this.alertCtrl.create({
+                title: "Error",
+                subTitle: "Bio length must be a minimum of 500 characters",
+                buttons: ["Got it :)"]
+            });
+            alert.present()
+        }
+        else {
+
+            this.navCtrl.pop();
+            this.userDatabase.updatePosts(this.postArray)
+        }
     }
 }
+
+
+
